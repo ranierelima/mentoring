81cae3a0f3204201cdce358215ca83f180fc0dd5
joaomarcus <joaomarcusjesus@gmail.com>
2017-05-19 11:43:25 -0300

    clean

--- app/Http/Controllers/App/EventosController.php
+++ app/Http/Controllers/App/EventosController.php
@@ -2,58 +2,82 @@
 
 namespace Mentor\Http\Controllers\App;
 
-
 use Illuminate\Http\Request;
 use Mentor\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-
-use Mentor\Models\Eventos;
-use Mentor\Models\User;
 use Mentor\Repositories\EventosRepositoryEloquent;
 use Mentor\Repositories\UserRepositoryEloquent;
+use Mockery\Exception;
 
 class EventosController extends Controller
 {
 	private $eventosRepository;
-	
     private $userRepository;
-	
-   /* public function __construct(EventosRepositoryEloquent $eventosRepository,
+
+    /**
+     * EventosController constructor.
+     * @param EventosRepositoryEloquent $eventosRepository
+     * @param UserRepositoryEloquent $userRepository
+     */
+    public function __construct(EventosRepositoryEloquent $eventosRepository,
                                 UserRepositoryEloquent $userRepository)
     {
-
-        $this->$eventosRepository = $eventosRepository;
+        $this->eventosRepository = $eventosRepository;
         $this->userRepository = $userRepository;
-    }*/
-	
-	public function index(){
-        $eventos = DB::select('select * from eventos order by id desc');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+
+        $eventos = $this->eventosRepository->paginate(5);
+
 		return view('eventos.index', compact('eventos'));
 	}
-	public function create(){
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
 		return view('eventos.cadastrar');
 	}
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
-		DB::table('eventos')->insert( [
+
+        /** Criar um service para isso... não misturar lógica com instâncias... toda persistência no bd usar try/catch */
+        try {
+            $this->eventosRepository->create([
                 'nome' => $request['nome'],
                 'local' => $request['local'],
                 'data_do_evento' => $request['data_do_evento'],
                 'telefone' => $request['telefone'],
                 'status' => 'pendente',
                 'user_id' => Auth::user()->id
-		] );
-//        $this->demandService->myDemandsCreate($request->all());
+            ]);
+
+        } catch(Exception $exception) {
+            $exception->getMessage();
+        }
 
         return redirect()->route('app.eventos.index');
     }
-	
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show($id)
     {
-//        $evento = $this->$eventosRepository->find($id);
-        $evento = DB::select('select * from eventos where id="'. $id .'" order by id desc')[0];
+        $evento = $this->eventosRepository->find($id);
+
 		return view('eventos.show', compact('evento'));
     }
 }
